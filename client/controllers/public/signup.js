619ef290f7d302d6ca8af0be897432a4a44dac67
Precious N. E. Nyarko <precious.ewusi.nyarko@meltwater.org>
2015-09-08 02:22:35 +0000

    Implement signup and login functionality

--- client/controllers/public/signup.js
+++ client/controllers/public/signup.js
@@ -1,77 +1,61 @@
 Template.signup.helpers({
-  accountCreationInProgress: function () {
-    return Session.get('accountCreationInProgress');
+  isLoggingIn: function () {
+    return Session.get('isLoggingIn');
   }
 });
 
 Template.signup.events({
   "submit .form": function (event) {
     event.preventDefault();
-    
+
     function resetForm() {
       $(event.target).trigger('reset');
     }
 
     var fullName    =     $('#full-name').val(),
       email       =     $('#email').val(),
-      companyName =     $('#company-name').val(),
+      telephone   =     $('#telephone').val(),
       password    =     $('#password').val();
 
-    if (!(fullName && email && companyName && password)) {
+    if (!(fullName && email && telephone && password)) {
       sAlert.warning("All fields are required. Please check your inputs.");
       return;
     } else {
       // Display spinner & disable text fields
-      Session.set('accountCreationInProgress', true);
+      Session.set('isLoggingIn', true);
 
       // Create the user's account.
       var user = {
         email: email,
         password: password,
         profile: {
-          fullName: fullName
+          fullName: fullName,
+          telephone: telephone,
+          userType: 'customer',
+          isAdmin: false
         }
       };
 
       Meteor.call('newUser', user, function (error, userId) {
         if (error) {
-          Session.set('accountCreationInProgress', false);
-          console.log('NError:', error);
+          Session.set('isLoggingIn', false);
+          console.log('Error:', error);
           if (error.error === 403) {
             sAlert.error("Sorry, a user with that email address already exists.");
           } else {
             sAlert.error('We had trouble processing your request.');
           }
         } else {
-          // Create Merchant
-          var shop = Session.get('shop'); // merchant shop url
-          var merchantInfo = {
-            name: companyName,
-            accountOwner: userId
-          };
-
-          if (shop) {
-            merchantInfo.shop = shop;
-          }
-
-          Meteor.call('createMerchant', merchantInfo, function (error) {
-            if (error) {
-              Session.set('accountCreationInProgress', false);
+          // Log the user in.
+          Meteor.loginWithPassword(user.email, user.password, function(error){
+            if(error){
+              Session.set('isLoggingIn', false);
               console.log('Error:', error);
-              sAlert.error('We had trouble processing your request.');
+              sAlert.error('Sorry. We had trouble logging you in.');
             } else {
-              // Log the user in.
-              Meteor.loginWithPassword(user.email, user.password, function(error){
-                if(error){
-                  Session.set('accountCreationInProgress', false);
-                  console.log('Error:', error);
-                  sAlert.error('Sorry. We had trouble logging you in.');
-                } else {
-                  Session.set('accountCreationInProgress', false);
-                  sAlert.success('Awesome! Your new account is ready to use.');
-                  resetForm();
-                }
-              });
+              Session.set('isLoggingIn', false);
+              sAlert.success('Awesome! Your new account is ready to use.');
+              resetForm();
             }
           });
         }
