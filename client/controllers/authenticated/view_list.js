a23c061a15bbaa5ce505cf9e50a014ef7df463a5
Precious N. E. Nyarko <precious.ewusi.nyarko@meltwater.org>
2015-09-11 00:17:07 +0000

    Implement controller for 'viewList' view
    This controller provides the functionality to change the name of the
    list, add and remove items, as well as updating existing items in the
    list.

---
+++ client/controllers/authenticated/view_list.js
@@ -0,0 +1,150 @@
+Template.viewList.onCreated(function() {
+  var self = this;
+  self.autorun(function() {
+    var listId = FlowRouter.getParam("id");
+    self.subscribe('packingUnits');
+    self.subscribe('aList', listId);
+    self.subscribe('listItems', listId);
+  });
+});
+
+Template.viewList.helpers({
+  showHeaderSpinner: function () {
+    return Session.get('updatingListName') || Session.get('removingItemFromList') || Session.get('savingChangesToAnItem');
+  },
+  hasItems: function () {
+    var listId = FlowRouter.getParam("id");
+    return !! Items.find({list: listId}).count();
+  },
+  list: function () {
+    var listId = FlowRouter.getParam("id");
+    return Lists.findOne({_id: listId});
+  },
+  listItems: function () {
+    var listId = FlowRouter.getParam("id");
+    return Items.find({list: listId});
+  },
+  showAddItemForm: function () {
+    return Session.get('showAddItemForm');
+  },
+  addingItemToList: function () {
+    return Session.get('addingItemToList');
+  },
+  isEditingItem: function (itemId) {
+    return Session.get('editingItem-' + itemId);
+  },
+  isSavingChangesToItem: function (itemId) {
+    return Session.get('savingChangesToItem-' + itemId);
+  }
+});
+
+Template.viewList.events({
+  "change #list-name": function (event) {
+    Session.set('updatingListName', true);
+    var list = {
+      name: $(event.target).val(),
+      id: FlowRouter.getParam("id")
+    };
+    Meteor.call('updateList', list, function (error) {
+      Session.set('updatingListName', false);
+      if (error) {
+        console.log('Error:', error);
+        sAlert.error(error.reason || 'Oops. We had trouble processing your last request.');
+        return;
+      } else {
+        console.log('update success...');
+      }
+    });
+  },
+  "click .show-add-item-form-trigger": function (event) {
+    Session.set('showAddItemForm', true);
+  },
+  "click .cancel-add-item": function (event) {
+    Session.set('showAddItemForm', false);
+  },
+  "click .add-item-btn": function (event) {
+    var item = {
+      description: $('#item-description').val(),
+      quantity: $('#item-quantity').val(),
+      packingUnit: $('#item-quantity-unit').val()
+    };
+
+    item.list = FlowRouter.getParam("id") || undefined;
+
+    if (!(item.description && item.quantity && item.packingUnit)) {
+      return sAlert.warning('All fields are required please.');
+    } else {
+      Session.set('addingItemToList', true);
+      Meteor.call('createItem', item, function (error, itemId) {
+        Session.set('addingItemToList', false);
+        if (error) {
+          console.log('Error:', error);
+          sAlert.error(error.reason || 'Oops. We had trouble processing your last request.');
+        } else {
+          var form = $(event.target).parents('.form')[0];
+          $(form).trigger('reset');
+          Session.set('showAddItemForm', false);
+        }
+      });
+    }
+  },
+  "click .edit-item-link": function (event) {
+    event.preventDefault();
+    Session.set('editingItem-' + this._id, true);
+  },
+  "click .cancel-edit-link": function (event) {
+    event.preventDefault();
+    Session.set('editingItem-' + this._id, false);
+  },
+  "click .save-changes-link": function (event) {
+    event.preventDefault();
+    var self = this; // self is the list item in question
+    if (Session.get('item-' + this._id + '-edited')) {
+      var item = {
+        id: this._id,
+        description: $('#new-item-description').val(),
+        quantity: $('#new-item-quantity').val(),
+        packingUnit: $('#item-quantity-unit').val()
+      };
+
+      if (!(item.description && item.quantity && item.packingUnit)) {
+        sAlert.warning('You have left some fields empty.');
+      } else {
+        Session.set('savingChangesToItem-' + this._id, true);
+        Session.set('savingChangesToAnItem', true);
+        Meteor.call('updateItem', item, function (error) {
+          Session.set('savingChangesToItem-' + self._id, false); // use self because 'this' is now fairy dust :)
+          Session.set('savingChangesToAnItem', false);
+          if (error) {
+            console.log('Error:', error);
+            sAlert.error(error.reason || 'Oops. We had trouble processing your last request.');
+          } else {
+            sAlert.success('Your changes were saved successfully.');
+            Session.set('editingItem-' + self._id, false);
+          }
+        });
+      }
+    } else {
+      Session.set('editingItem-' + this._id, false);
+    }
+  },
+  "click .remove-item-link": function (event) {
+    event.preventDefault();
+    if (confirm('Click OK to confirm. Or cancel to return.')) {
+      Session.set('removingItemFromList', true);
+      Meteor.call('removeItem', this._id, function (error) {
+        Session.set('removingItemFromList', false);
+        if (error) {
+          console.log('Error:', error);
+          sAlert.error(error.reason || 'Oops. We had trouble processing your last request.');
+          return;
+        } else {
+          sAlert.success('An item was successfully removed from this list.');
+        }
+      });
+    }
+  },
+  "change #new-item-description, change #new-item-quantity, change #item-quantity-unit": function (event) {
+    Session.set('item-' + this._id + '-edited', true);
+  }
+});
--- server/methods/items.js
+++ server/methods/items.js
@@ -22,5 +22,63 @@ Meteor.methods({
     }
 
     return Items.insert(item);
+  },
+  updateItem: function (item) {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    if (item && item.quantity) {
+      item.quantity = parseInt(item.quantity); 
+    }
+
+    try {
+      check(item, {
+        id: String,
+        description: String,
+        quantity: Number,
+        packingUnit: String
+      });
+    } catch (e) {
+      throw new Meteor.Error(e.message);
+    }
+
+    var target = Items.findOne(item.id);
+    if (!target) {
+      throw new Meteor.Error('not-found', 'We couldn\'t understand your last request.');
+    } else {
+      var targetList = Lists.findOne(target.list);
+      if (targetList.owner !== Meteor.userId()) {
+        throw new Meteor.Error('not-authorized', 'You cannot edit another user\'s list items');
+      }
+    }
+
+    var itemId = item.id; delete item.id;
+    Items.update({_id: itemId}, {$set: item});
+
+    return;
+  },
+  removeItem: function (itemId) {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    try {
+      check(itemId, String);
+    } catch (e) {
+      throw new Meteor.Error(e.message);
+    }
+
+    var item = Items.findOne(itemId);
+    if (!item) {
+      throw new Meteor.Error('not-found');
+    }
+
+    var list = Lists.findOne({_id: item.list});
+    if (list.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    return Items.remove({_id: itemId});
   }
 });
