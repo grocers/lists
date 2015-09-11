053d0e5987f2dfe2fa1941052f2cc6be7ef30e0e
Precious N. E. Nyarko <precious.ewusi.nyarko@meltwater.org>
2015-09-11 00:11:39 +0000

    Publish single lists using the list id

--- server/publications/lists.js
+++ server/publications/lists.js
@@ -4,17 +4,22 @@
 */
 
 Meteor.publish('currentUserLists', function(){
-  // If need be, Meteor gives us access to the current user via this.userId.
-  // Example below shows using this.userId to locate documents where the
-  // owner field is equal to a userId. Additionally, a fields projection is
-  // added to specify which fields you want to return (where 1 = true and
-  // 0 = false).
-
   var user = this.userId;
   if (!user) {
     return [];
   }
   var data = Lists.find({"owner": user});
+
+  return data;
+});
+
+Meteor.publish('aList', function(listId){
+  var user = this.userId;
+  if (!user) {
+    return null;
+  }
+
+  var data = Lists.find({_id: listId});
 
   return data;
 });
