3e292013bbbc7a47c9381db54fdd7eb192b1933f
Precious N. E. Nyarko <precious.ewusi.nyarko@meltwater.org>
2015-09-17 22:34:30 +0000

    Route to admin section if logged in user is admin
    On successful login, I now check for the presence of an admin user. If
    one is found, we route to the admin section or to the regular lists view
    otherwise.

--- client/utilities/session.js
+++ client/utilities/session.js
@@ -1,6 +1,17 @@
 Accounts.onLogin(function () {
+  console.log('runningOnlogin', redirectTo);
   var redirectTo = Session.get('redirectAfterLogin');
-  if (redirectTo && redirectTo !== 'login') {
+  if (redirectTo) {
+    if (redirectTo !== 'login') {
+      return FlowRouter.go(redirectTo);
+    }
+  } else {
+    var user = Meteor.user();
+    if (user.profile.isAdmin) {
+      redirectTo = 'adminOverview';
+    } else {
+      redirectTo = 'lists';
+    }
     return FlowRouter.go(redirectTo);
   }
 });
