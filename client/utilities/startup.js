9b313fe6190853f7e1cd3c3e7fa9c493065dae97
Precious N. E. Nyarko <precious.ewusi.nyarko@meltwater.org>
2015-09-17 23:10:01 +0000

    Float viewport to the right when affix kicks in
    This is required because when affix-top is active, the left menu uses
    fixed positioning, which causes the viewport on the right to overlap the
    menu. This update ensures that the viewport stays in place.

--- client/utilities/startup.js
+++ client/utilities/startup.js
@@ -1,13 +1,23 @@
 Meteor.startup(function () {
 
-    sAlert.config({
-        effect: 'stackslide',
-        position: 'bottom-right',
-        timeout: 5000,
-        html: false,
-        onRouteClose: true,
-        stack: true,
-        offset: 0
-    });
+  sAlert.config({
+    effect: 'stackslide',
+    position: 'bottom-right',
+    timeout: 5000,
+    html: false,
+    onRouteClose: true,
+    stack: true,
+    offset: 0
+  });
+
+  $(window).scroll(function () {
+    var pos = $(window).scrollTop();
+    if (pos <= 40) {
+      // normal styling
+      $('.viewport').removeClass('floated');
+    } else {
+      $('.viewport').addClass('floated');
+    }
+  });
 
 });
