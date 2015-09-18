14b2bb41fd39f3d6ccb40aee3b91e8127a4957fe
Precious N. E. Nyarko <precious.ewusi.nyarko@meltwater.org>
2015-09-18 11:16:52 +0000

    Implement form to allow customers send feedback
    Customers may use this to send feedback or request support.
    In this update, I also ensure that only customers (not admins) see this
    feedback form.

--- client/controllers/authenticated/app_navigation.js
+++ client/controllers/authenticated/app_navigation.js
@@ -1,5 +1,49 @@
 Template.appNavigation.helpers({
   username: function () {
-    return Meteor.user().profile.fullName;
+    var user = Meteor.user();
+    if (!user) {
+      return '';
+    } else {
+      return user.profile.fullName;
+    }
+  },
+  sendingFeedback: function () {
+    return Session.get('sendingFeedback');
+  },
+  isCustomer: function () {
+    var user = Meteor.user();
+    if (!user) {
+      return false;
+    } else {
+      return !user.profile.isAdmin;
+    }
+  }
+});
+
+Template.appNavigation.events({
+  "submit .feedback-form": function (event) {
+    event.preventDefault();
+
+    var inquiry = $('.help #message').val();
+    if (!inquiry) {
+      return sAlert.warning("Please enter a message to send.");
+    }
+
+    var message = {
+      user: Meteor.userId(),
+      text: inquiry
+    };
+
+    Session.set('sendingFeedback', true);
+    Meteor.call('createMessage', message, function (error) {
+      Session.set('sendingFeedback', false);
+      if (error) {
+        console.log('Error:', error);
+        return sAlert.error(error.reason || 'Oops. We had trouble processing your last request.');
+      } else {
+        $(event.target).trigger('reset');
+        sAlert.success('Your message was sent successfully.');
+      }
+    });
   }
 });
