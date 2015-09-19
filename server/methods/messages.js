3e42b3e390729a86e2d688aedcfe1df130a13c98
Precious N. E. Nyarko <precious.ewusi.nyarko@meltwater.org>
2015-09-19 10:53:02 +0000

    Add functionality to mark a message as read
    Also including the sender's name in the message document, so we don't
    have to run a separate query (using the user's id) just to figure this
    out.

--- server/methods/messages.js
+++ server/methods/messages.js
@@ -11,8 +11,31 @@ Meteor.methods({
       text: String
     });
 
+    var sender = Meteor.users.findOne(message.user);
+    if (!sender) {
+      throw new Meteor.Error('not-found', 'User not found');
+    }
+
     message.createdAt = new Date();
     message.read = false;
+    message.userName = sender.profile.fullName;
     return Messages.insert(message);
+  }
+});
+
+Meteor.methods({
+  markAsRead: function (messageId) {
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    try {
+      check(messageId, String);
+    } catch (e) {
+      throw new Meteor.Error('bad-input', 'Invalid value supplied for parameter.');
+    }
+
+    Messages.update({_id: messageId}, {$set: {read: true, updatedAt: new Date()}});
+    return;
   }
 });
