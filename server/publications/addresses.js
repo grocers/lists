2ae6ec0fa74db0c48c308b6ace7c11a498c09968
Precious N. E. Nyarko <precious.ewusi.nyarko@meltwater.org>
2015-09-23 15:28:26 +0000

    Permit fetching address for a given customer
    Only constraint being that, only the owner of the address or an admin is
    permitted to use this information.

--- server/publications/addresses.js
+++ server/publications/addresses.js
@@ -3,12 +3,36 @@
  * Data publications for the Addresses collection.
 */
 
-Meteor.publish('loggedInShopperAddress', function(listId) {
+Meteor.publish('loggedInShopperAddress', function() {
   var user = this.userId;
   if (!user) {
     return [];
   }
 
   var data = Addresses.find({"user": user});
+  return data;
+});
+
+Meteor.publish('shopperAddress', function(shopperId) {
+  var userId = this.userId;
+  if (!userId) {
+    return [];
+  }
+
+  var currentUser = Meteor.users.findOne(userId);
+  if (!currentUser) {
+    return [];
+  }
+
+  var target = Addresses.findOne({"user": shopperId});
+  if (!target) {
+    return [];
+  }
+
+  if (target.user !== currentUser._id && !currentUser.profile.isAdmin) {
+   return []; 
+  }
+
+  var data = Addresses.find({"user": shopperId});
   return data;
 });
