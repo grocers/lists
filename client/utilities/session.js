84a122cf45e9dc926fa79e40c533ddb0023fed41
Precious N. E. Nyarko <precious.ewusi.nyarko@meltwater.org>
2015-10-03 17:20:49 +0000

    Redirect logged in users to an authenticated route
    This is for cases, where a logged in user visits a public route.

--- client/utilities/session.js
+++ client/utilities/session.js
@@ -2,20 +2,22 @@ Accounts.onLogin(function () {
   var user = Meteor.user();
   var redirectTo = Session.get('redirectAfterLogin');
 
+  var currentRoute = FlowRouter.current().route;
+
   if (redirectTo) {
     if (redirectTo !== 'login') {
       Session.set('redirectAfterLogin', null);
       return FlowRouter.go(redirectTo);
     }
   } else {
-    var currentPath = window.location.pathname;
-    if (currentPath === '/login') {
+    if (Route.isPublicRoute(currentRoute)) {
       if (user.profile.isAdmin) {
         FlowRouter.go('adminOverview');
       } else {
         FlowRouter.go('lists');
       }
     } else {
+      var currentPath = window.location.pathname;
       FlowRouter.go(currentPath);
     }
   }
